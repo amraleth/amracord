d8ebeac881a90e44576805e47f4bb0ec4e593e8a
Amraleth <amraleth@proton.me>
2024-07-09 16:01:01 +0200

    Add javadocs for ConfigProvider

--- src/main/java/xyz/amraleth/amracord/test/config/ConfigProvider.java
+++ src/main/java/xyz/amraleth/amracord/test/config/ConfigProvider.java
@@ -7,6 +7,11 @@ import org.jetbrains.annotations.NotNull;
 import java.io.FileReader;
 import java.io.IOException;
 
+/**
+ * Provides an easy way for loading json configs
+ *
+ * @author amraleth
+ */
 public class ConfigProvider {
     private final Gson gson;
 
@@ -14,8 +19,16 @@ public class ConfigProvider {
         this.gson = new Gson();
     }
 
-    public <T> T loadConfig(@NotNull String config, @NotNull Class<T> configClazz) {
-        try (JsonReader jsonReader = new JsonReader(new FileReader(config))) {
+    /**
+     * Loads the json configuration
+     *
+     * @param configPath  The path to the configuration file
+     * @param configClazz The class to parse the config to
+     * @param <T>         The config class
+     * @return An instance of the configClazz
+     */
+    public <T> T loadConfig(@NotNull String configPath, @NotNull Class<T> configClazz) {
+        try (JsonReader jsonReader = new JsonReader(new FileReader(configPath))) {
             return this.gson.fromJson(jsonReader, configClazz);
         } catch (IOException e) {
             throw new RuntimeException(e);
